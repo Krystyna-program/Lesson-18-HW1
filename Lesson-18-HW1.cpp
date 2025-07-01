3678835d016f7e1056e8d2815b92d9c09e34de9a
Krystyna Khondak <kristinakhondak@gmail.com>
2025-07-01 14:47:49 +0300

    updated users for 60

--- Lesson-18-HW1.cpp
+++ Lesson-18-HW1.cpp
@@ -3,44 +3,46 @@
 
 int main()
 {
-    Employee arr[5];
-    const int CURRENT_YEAR = 2025; 
+    const int SIZE = 5;
+    const int CURRENT_YEAR = 2025;
+    Employee arr[SIZE];
     int countOlderThan60 = 0;
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < SIZE; i++)
     {
+        cout << "--- Enter data for employee #" << (i + 1) << " ---\n";
+
         cout << "Enter last name: ";
-        cin.ignore();
         cin.getline(arr[i].lastName, 100);
 
         cout << "Enter year of birth: ";
         cin >> arr[i].yearOfBirth;
+        cin.ignore(1000, '\n'); 
 
         cout << "Enter position: ";
-        cin.ignore();
         cin.getline(arr[i].position, 100);
 
         cout << "Enter salary: ";
         cin >> arr[i].salary;
+        cin.ignore(1000, '\n');  
 
         cout << "Enter education: ";
-        cin.ignore();
         cin.getline(arr[i].education, 100);
     }
-
-    cout << "\nEmployees older than 60:\n";
-    for (int i = 0; i < 5; i++)
+    cout << "\n=== Employees older than 60 ===\n";
+    for (int i = 0; i < SIZE; i++)
     {
         int age = CURRENT_YEAR - arr[i].yearOfBirth;
         if (age > 60)
         {
             countOlderThan60++;
-            cout << "Last name: " << arr[i].lastName << ", ";
-            cout << "Year of birth: " << arr[i].yearOfBirth << ", ";
-            cout << "Age: " << age << ", ";
-            cout << "Position: " << arr[i].position << ", ";
-            cout << "Salary: " << arr[i].salary << ", ";
-            cout << "Education: " << arr[i].education << endl;
+            cout << "\nEmployee #" << (i + 1) << ":\n";
+            cout << "Last name: " << arr[i].lastName << "\n";
+            cout << "Year of birth: " << arr[i].yearOfBirth << "\n";
+            cout << "Age: " << age << "\n";
+            cout << "Position: " << arr[i].position << "\n";
+            cout << "Salary: " << arr[i].salary << "\n";
+            cout << "Education: " << arr[i].education << "\n";
         }
     }
     cout << "\nTotal employees older than 60: " << countOlderThan60 << endl;

b4bcc1846844fc6ff48cae9bea5ad914f6f61cb4
Krystyna Khondak <kristinakhondak@gmail.com>
2025-07-01 14:17:33 +0300

    employees over 60

--- Lesson-18-HW1.cpp
+++ Lesson-18-HW1.cpp
@@ -3,34 +3,45 @@
 
 int main()
 {
-	Employee arr[5];
+    Employee arr[5];
+    const int CURRENT_YEAR = 2025; 
+    int countOlderThan60 = 0;
+
     for (int i = 0; i < 5; i++)
     {
         cout << "Enter last name: ";
-        cin.ignore();  
+        cin.ignore();
         cin.getline(arr[i].lastName, 100);
 
         cout << "Enter year of birth: ";
         cin >> arr[i].yearOfBirth;
 
         cout << "Enter position: ";
-        cin.ignore();  
+        cin.ignore();
         cin.getline(arr[i].position, 100);
 
         cout << "Enter salary: ";
         cin >> arr[i].salary;
 
         cout << "Enter education: ";
-        cin.ignore(); 
+        cin.ignore();
         cin.getline(arr[i].education, 100);
     }
-    cout << "\nData from array:\n";
+
+    cout << "\nEmployees older than 60:\n";
     for (int i = 0; i < 5; i++)
     {
-        cout << "Last name: " << arr[i].lastName << ", ";
-        cout << "Year of birth: " << arr[i].yearOfBirth << ", ";
-        cout << "Position: " << arr[i].position << ", ";
-        cout << "Salary: " << arr[i].salary << ", ";
-        cout << "Education: " << arr[i].education << endl;
+        int age = CURRENT_YEAR - arr[i].yearOfBirth;
+        if (age > 60)
+        {
+            countOlderThan60++;
+            cout << "Last name: " << arr[i].lastName << ", ";
+            cout << "Year of birth: " << arr[i].yearOfBirth << ", ";
+            cout << "Age: " << age << ", ";
+            cout << "Position: " << arr[i].position << ", ";
+            cout << "Salary: " << arr[i].salary << ", ";
+            cout << "Education: " << arr[i].education << endl;
+        }
     }
+    cout << "\nTotal employees older than 60: " << countOlderThan60 << endl;
 }

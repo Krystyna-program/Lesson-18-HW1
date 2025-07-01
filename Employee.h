d5e57e8c8cd75fdf4e3196abd7bf31d82db92dda
Krystyna Khondak <kristinakhondak@gmail.com>
2025-07-01 12:51:00 +0300

    add struct Employee

---
+++ Employee.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+struct Employee {
+	char lastName[100];
+	int yearOfBirth;
+	char position[100];
+	float salary;
+	char education[100];
+};
